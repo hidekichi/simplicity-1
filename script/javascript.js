6c45638fb57756419c94f94bc952823423b2b8d5
hidekichi <hidekichi0513@gmail.com>
2016-06-22 17:09:11 +0900

    同じアドレスのリロードで履歴記録キャンセル

--- script/javascript.js
+++ script/javascript.js
@@ -3,28 +3,28 @@
 
 	function prettyDate(time) {
 		var
-		date = new Date((time || "").replace(/-/g, "/").replace(/[TZ]/g, " ")),
-		diff = (((new Date()).getTime() - date.getTime()) / 1000),
-		day_diff = Math.floor(diff / 86400);
+			date = new Date((time || "").replace(/-/g, "/").replace(/[TZ]/g, " ")),
+			diff = (((new Date()).getTime() - date.getTime()) / 1000),
+			day_diff = Math.floor(diff / 86400);
 
 		if (isNaN(day_diff) || day_diff < 0 || day_diff >= 31) return;
 
 		return day_diff === 0 && (
-			diff < 30 && "ほぼ今" ||
-			diff < 60 && "ちょい前" ||
-			diff < 120 && "1 分前" ||
-			diff < 3600 && Math.floor(diff / 60) + " 分前" ||
-			diff < 7200 && "1 時間前" ||
-			diff < 86400 && Math.floor(diff / 3600) + " 時間前"
-		) ||
-		day_diff === 1 && "昨日" ||
-		day_diff < 7 && day_diff + " 日前" ||
-		day_diff < 31 && Math.ceil(day_diff / 7) + " 週間前";
+				diff < 30 && "ほぼ今" ||
+				diff < 60 && "ちょい前" ||
+				diff < 120 && "1 分前" ||
+				diff < 3600 && Math.floor(diff / 60) + " 分前" ||
+				diff < 7200 && "1 時間前" ||
+				diff < 86400 && Math.floor(diff / 3600) + " 時間前"
+			) ||
+			day_diff === 1 && "昨日" ||
+			day_diff < 7 && day_diff + " 日前" ||
+			day_diff < 31 && Math.ceil(day_diff / 7) + " 週間前";
 	}
 
 	var
-	//object = localStorage.getItem('access_data') ? JSON.parse(localStorage.getItem('access_data')) : { items: [] };
-	object = sessionStorage.getItem('access_data') ? JSON.parse(sessionStorage.getItem('access_data')) : { items: [] };
+		//object = localStorage.getItem('access_data') ? JSON.parse(localStorage.getItem('access_data')) : { items: [] };
+		object = sessionStorage.getItem('access_data') ? JSON.parse(sessionStorage.getItem('access_data')) : { items: [] };
 
 	var setLocalStorage = function(itemnum) {
 
@@ -35,17 +35,17 @@
 		var maxItem = parseInt(itemnum, 10);
 
 		var
-		thisUrl = window.location.href,
-		url = thisUrl,
-		entryTitle = $(".entry-title").text(),
-		title = $.trim(entryTitle),
-		thumb,
-		modified;
+			thisUrl = window.location.href,
+			url = thisUrl,
+			entryTitle = $(".entry-title").text(),
+			title = $.trim(entryTitle),
+			thumb,
+			modified;
 
 		//アイキャッチがない場合はカラ
 		if ($(".eye-catch").find("img").length) {
 			var imgSrc = $(".eye-catch").find("img").attr("src"),
-			reimgSrc = imgSrc.replace(".jpg", "-100x100.jpg");
+				reimgSrc = imgSrc.replace(".jpg", "-100x100.jpg");
 			thumb = reimgSrc;
 		}
 		else {
@@ -67,8 +67,8 @@
 		}
 
 		var
-		archiveTitle = $("#archive-title").text(),
-		trimArchiveTitle = archiveTitle.replace(/\n|\s+/g, "");
+			archiveTitle = $("#archive-title").text(),
+			trimArchiveTitle = archiveTitle.replace(/\n|\s+/g, "");
 		if ($("body").hasClass("category") || $("body").hasClass("date")) {
 			var cat_title = $.trim(trimArchiveTitle);
 			title = "カテゴリ(" + cat_title + ")";
@@ -93,25 +93,41 @@
 		data.modified = modified;
 		data.time = time;
 
-		if (object.items.length >= maxItem) {
-			object.items.shift();
+		var itemLength = object.items.length;
+
+		if ( itemLength > 0) {
+			if ( object.items[itemLength - 1].url === data.url ) {
+				return false;
+			} else {
+				if (itemLength >= maxItem) {
+					object.items.shift();
+				}
+
+				//データ追加
+				object.items.push(data);
+			}
+
+			//LSにデータ保管
+			//window.localStorage.setItem("access_data", JSON.stringify(object));
+			window.sessionStorage.setItem("access_data", JSON.stringify(object));
+
+		} else {
+			//データ追加
+			object.items.push(data);
+
+			//LSにデータ保管
+			//window.localStorage.setItem("access_data", JSON.stringify(object));
+			window.sessionStorage.setItem("access_data", JSON.stringify(object));
 		}
 
-		//データ追加
-		object.items.push(data);
-
-		//LSにデータ保管
-		//window.localStorage.setItem("access_data", JSON.stringify(object));
-		window.sessionStorage.setItem("access_data", JSON.stringify(object));
-
+		return false;
 	};
 
 	var getLocalStorage = function() {
+		//if (localStorage.getItem('access_data')) {
 		if (sessionStorage.getItem('access_data')) {
-			//if (localStorage.getItem('access_data')) {
-			var json = JSON.parse(sessionStorage.getItem('access_data'));
 			//var json = JSON.parse(localStorage.getItem('access_data'));
-
+			var json = JSON.parse(sessionStorage.getItem('access_data'));
 			return json;
 		}
 	};
@@ -127,35 +143,35 @@
 			//リスト表示部分
 
 			var target = $("#b-history"),
-			json = getLocalStorage(),
-			labeltext = "閲覧履歴";
+				json = getLocalStorage(),
+				labeltext = "閲覧履歴";
 
 			//ラベル設定
 			if (!target.closest(".widget_text").children("h3").length && !target.closest(".widget_text").children("h3").text().trim.length) {
 				target.closest(".widget_text")
-				.prepend("<h3 class='widget_title sidebar_widget_title'>")
-				.find(".sidebar_widget_title")
-				.text(labeltext);
+					.prepend("<h3 class='widget_title sidebar_widget_title'>")
+					.find(".sidebar_widget_title")
+					.text(labeltext);
 			}
 
 			//リスト設定
 			target.append("<ul>");
 
 			var list = target.find("ul");
-			
+
 			if (json){
 				var jdat = json.items;
 				$.each(jdat, function(ji) {
 					list
-					.prepend("<li><a href='" + jdat[ji].url + "' title='" + jdat[ji].title + "'>" +
-					"<div class='wrapper'>" +
-					"<div class='thumb'><img src='" + jdat[ji].thumb + "' /></div>" +
-					"<div class='title'>" + jdat[ji].title + "</div>" +
-					"<div class='times'>" +
-					"<div class='date'>" + jdat[ji].modified + "</div>" +
-					"<div class='recTime' data-time='"+jdat[ji].time+"'>" + jdat[ji].time + "</div>" +
-					"</div>" +
-					"</div></a></li>");
+						.prepend("<li><a href='" + jdat[ji].url + "' title='" + jdat[ji].title + "'>" +
+							"<div class='wrapper'>" +
+							"<div class='thumb'><img src='" + jdat[ji].thumb + "' /></div>" +
+							"<div class='title'>" + jdat[ji].title + "</div>" +
+							"<div class='times'>" +
+							"<div class='date'>" + jdat[ji].modified + "</div>" +
+							"<div class='recTime' data-time='"+jdat[ji].time+"'>" + jdat[ji].time + "</div>" +
+							"</div>" +
+							"</div></a></li>");
 				});
 
 				var recTime = $(".recTime");
@@ -180,6 +196,7 @@
 					},800);
 				});
 			}
+
 		}
 	});
 })(jQuery);
